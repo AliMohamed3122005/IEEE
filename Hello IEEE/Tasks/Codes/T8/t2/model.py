b6728327013d63b2422ede3a7f7858cd044151cd
Ali Mohamed <alymohamed220055@gmail.com>
2026-07-09 18:23:35 +0300

    t8-t2

--- Hello IEEE/Tasks/Codes/T8/t2/model.py
+++ Hello IEEE/Tasks/Codes/T8/t2/model.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import f1_score
 
 def train_model(df):
@@ -25,14 +25,14 @@ def train_model(df):
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    iterations_values=range(1000,10001,1000)
+    learning_rates=np.arange(0.001,1.001,0.001)
     train_f1_scores = []
     test_f1_scores=[]
-    best_iter=None
+    best_lr=None
     best_test_f1=0
 
-    for max_iter in iterations_values:
-        model = LogisticRegression(max_iter=max_iter,solver="lbfgs")
+    for lr in learning_rates:
+        model = SGDClassifier(loss="log_loss",learning_rate="constant",eta0=lr,max_iter=1000,random_state=42)
         model.fit(X_train,y_train)
         y_train_pred = model.predict(X_train)
         y_test_pred = model.predict(X_test)
@@ -45,16 +45,16 @@ def train_model(df):
 
         if test_f1 > best_test_f1:
             best_test_f1=test_f1
-            best_iter= max_iter
+            best_lr= lr
 
-    print("Best iteraions:",best_iter)
+    print("Best Learning ratr:",best_lr)
     print("best test F1 score:",best_test_f1)
 
     plt.figure()
-    plt.plot(iterations_values,train_f1_scores,label="Training F1 score")
-    plt.plot(iterations_values,test_f1_scores,label="Test F1 score")
+    plt.plot(learning_rates,train_f1_scores,label="Training F1 score")
+    plt.plot(learning_rates,test_f1_scores,label="Test F1 score")
 
-    plt.xlabel("Number of iterations")
+    plt.xlabel("Learning Rate")
     plt.ylabel("F1-Scores")
     plt.legend()
     plt.grid(True)
